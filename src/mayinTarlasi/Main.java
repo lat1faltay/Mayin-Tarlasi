41f09c29dd95278f7737276d70e7cb6d4d4fc0e8
Latif Altay <latifaltay00@gmail.com>
2023-04-14 22:54:53 +0300

    first commit

---
+++ src/mayinTarlasi/Main.java
@@ -0,0 +1,12 @@
+package mayinTarlasi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MayinTarlasi oyun = new MayinTarlasi();
+
+	}
+
+}
---
+++ src/mayinTarlasi/MayinTarlasi.java
@@ -0,0 +1,16 @@
+package mayinTarlasi;
+
+import javax.swing.JFrame;
+
+public class MayinTarlasi {
+	JFrame frame;
+	
+	public MayinTarlasi() {
+		frame = new JFrame("Mayın Tarlası");
+		
+		frame.setSize(800,800);
+		frame.setVisible(true);
+		frame.setResizable(false);
+	}
+	
+}
