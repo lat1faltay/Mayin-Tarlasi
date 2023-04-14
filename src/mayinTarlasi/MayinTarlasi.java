d9f31814d01333b2e15676eed44e666b9c674e71
Latif Altay <latifaltay00@gmail.com>
2023-04-14 23:43:27 +0300

    commit_2_buttonsCreated

--- src/mayinTarlasi/MayinTarlasi.java
+++ src/mayinTarlasi/MayinTarlasi.java
@@ -1,15 +1,29 @@
 package mayinTarlasi;
 
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
 
 public class MayinTarlasi {
 	JFrame frame;
+	Btn[][] board = new Btn[10][10];
 	
 	public MayinTarlasi() {
 		frame = new JFrame("Mayın Tarlası");
 		
 		frame.setSize(800,800);
 		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridLayout(10,10));
+		
+		for(int row=0; row < board.length; row++) {
+			for(int col = 0; col < board[0].length; col++) {
+				Btn b = new Btn(row,col);
+				frame.add(b);
+				board[row][col] = b;
+			}
+		}
+		
 		frame.setResizable(false);
 	}
 	
